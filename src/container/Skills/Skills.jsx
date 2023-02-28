3db486dafc117b79abc0e9a7938c376fa9009fec
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-02-28 22:17:56 +1100

    final animation with viewport fixed

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -29,12 +29,12 @@ const Skills = () => {
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills.map((skill, i) => (
+          {skills?.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill.name + i}
+              key={skill.name}
             >
               <div
                 className="app__flex"
@@ -53,7 +53,7 @@ const Skills = () => {
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience.works.map((work, i) => (
+                {experience.works.map((work) => (
                   <>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
@@ -61,7 +61,7 @@ const Skills = () => {
                       className="app__skills-exp-work"
                       data-tip
                       data-for={work.name}
-                      key={work.name + i}
+                      key={work.name}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
